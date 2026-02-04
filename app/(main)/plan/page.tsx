7cf534539e042a19cbeab2bd0912cb1c1d5c6fc9
Marta <martinha_peres@hotmail.com>
2026-02-04 16:38:56 +0000

    Refactor HighlightsSection into reusable IconGridSection and add travel kit to plan page

--- app/(main)/plan/page.tsx
+++ app/(main)/plan/page.tsx
@@ -1,9 +1,15 @@
 import PlanHeroSection from "../../components/PlanHeroSection";
+import IconGridSection from "../../components/IconGridSection";
+import { travelKitItems } from "../../data/travelKitData";
 
 export default function PlanPage() {
   return (
     <main>
       <PlanHeroSection />
+      <IconGridSection
+        heading="Kit do Viajante"
+        items={travelKitItems}
+      />
     </main>
   );
 }
